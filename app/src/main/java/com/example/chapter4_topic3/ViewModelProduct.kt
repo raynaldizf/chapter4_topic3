79f3e2dd92fb18e6ee5533125ae50a52102380ee
surezluvy <raynaldizoel@gmail.com>
2022-09-19 15:00:54 +0700

    Add Image Product

--- app/src/main/java/com/example/chapter4_topic3/ViewModelProduct.kt
+++ app/src/main/java/com/example/chapter4_topic3/ViewModelProduct.kt
@@ -5,9 +5,9 @@ import androidx.lifecycle.ViewModel
 
 class ViewModelProduct : ViewModel() {
     val listProduct = arrayListOf(
-        ProductData("Cocacola",R.drawable.ic_launcher_background,6000,20,"Minuman Bersoda Mantap"),
-        ProductData("Sprite",R.drawable.ic_launcher_background,6500,25,"Minuman Bersoda Mantap"),
-        ProductData("Kopi Kapal Api",R.drawable.ic_launcher_background,3000,16,"Kopi Mantap Gak Bikin Ngantuk"),
+        ProductData("Cocacola",R.drawable.cocacola,6000,20,"Minuman Bersoda Mantap"),
+        ProductData("Sprite",R.drawable.sprite,6500,25,"Minuman Bersoda Mantap"),
+        ProductData("Kopi Kapal Api",R.drawable.kapalapi,3000,16,"Kopi Mantap Gak Bikin Ngantuk"),
     )
     val productList: MutableLiveData<List<ProductData>> = MutableLiveData()
 
